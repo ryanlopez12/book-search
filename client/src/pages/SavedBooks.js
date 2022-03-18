961d0760f49ca2b5f918bc31b3e73c4160a25a09
Seeky <seek916@gmail.com>
2022-03-17 19:59:07 -0700

    edit json with legacy code because of nodejs 17

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,42 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_ME} from '../utils/queries'
+import {REMOVE_BOOK} from '../utils/mutations'
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const {loading, data} = useQuery(QUERY_ME)
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const [removeBook, {err}] = useMutation(REMOVE_BOOK)
 
-        if (!token) {
-          return false;
-        }
+  const userData = data?.me || {}
 
-        const response = await getMe(token);
+  console.log(data)
 
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
+  console.log(userData)
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -45,23 +28,19 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      const {data} = await removeBook({
+        variables: {bookId}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
+      
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
+
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
@@ -74,12 +53,12 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
+          {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
--- server/server.js
+++ server/server.js
@@ -1,37 +1,82 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
-const {typDefs, resolvers} =('./schemas');
-const {authMiddleware} = require('./utils/auth');
-
+const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
+
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware,
+});
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
-});
+  })
+  };
 
-startServer(typeDefs, resolvers)
+startApolloServer(typeDefs, resolvers)
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// const express = require('express');
+// const {ApolloServer} = require('apollo-server-express');
+// const path = require('path');
+// const db = require('./config/connection');
+// // const routes = require('./routes');
+// const {typDefs, resolvers} =('./schemas');
+// const {authMiddleware} = require('./utils/auth');
 
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-}
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
-});
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const startServer = async () => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware,
+//   });
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//     })
+// });
+
+// startServer(typeDefs, resolvers)
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// // Serve up static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+// }
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/'));
+// });
